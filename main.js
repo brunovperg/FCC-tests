cbca3aa829340a46703024a2919b6fd807e8daf3
BRUNO PERGENTINO <brunovpergentino@gmail.com>
2023-01-06 18:58:28 -0300

    test 2

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function checkCashRegister(price, cash, cid) {
     return change;
   }
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  checkCashRegister(19.5, 20, [["PENNYy", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
 
 

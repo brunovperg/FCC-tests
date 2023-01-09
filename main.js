d5982b487989e7009ebae17d1974679597d699c2
Bruno Pergentino <brunovpergentino@gmail.com>
2023-01-09 15:43:22 -0300

    finish

--- main.js
+++ main.js
@@ -1,24 +1,72 @@
-const denom = [
-    { name: 'PENNY', val: 0.01}
-	{ name: 'NICKEL', val: 0.05},
-	{ name: 'DIME', val: 0.1},
-	{ name: 'QUARTER', val: 0.25},
-	{ name: 'ONE', val: 1},
-	{ name: 'FIVE', val: 5},
-	{ name: 'TEN', val: 10},
-	{ name: 'TWENTY', val: 20},
-    { name: 'ONE HUNDRED', val: 100},
-]
+// let convertToRoman = function(num){
+//     let decimalValue = [1000, 900, 500, 400, 100, 90,  50, 40, 10, 9, 5, 4, 1]
+//     var romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
-function checkCashRegister(price, cash, cid) {
-    let output = {status: null, change: []}
-    let change = cash - price
-    let register = cid.reduce(function(acc, curr))
-    let change;
-    return change;
-  }
+//     let romanized = ''
+
+//     for(let index = 0 ; index < decimalValue.length; index++){
+//         while(decimalValue[index] <= num){
+//             romanized += romanNumeral[index]
+//             num -= decimalValue[index]
+//         }
+//     }
+//     return romanized
+// }
+// console.log(convertToRoman(3999))
+
+
+// const denom = [
+//     { name: 'ONE HUNDRED', val: 100},
+// 	{ name: 'TWENTY', val: 20},
+// 	{ name: 'TEN', val: 10},
+// 	{ name: 'FIVE', val: 5},
+// 	{ name: 'ONE', val: 1},
+// 	{ name: 'QUARTER', val: 0.25},
+// 	{ name: 'DIME', val: 0.1},
+// 	{ name: 'NICKEL', val: 0.05},
+//     { name: 'PENNY', val: 0.01}
+// ]
+
+// function checkCashRegister(price, cash, cid) {
+//     let output = {status: null, change: []}
+//     let change = cash - price
+//     let register = cid.reduce(function(acc, curr){
+//         acc.total += curr[1]
+//         acc[curr[0]] = curr[1]
+//         return acc
+//     },{total: 0})
+//     if(register.total === change){
+//         output.status = 'CLOSED'
+//         output.change = cid
+//         return output
+//     }
+//     if(register.total < change){
+//         output.status = 'INSUFFICIENT_FUNDS'
+//         return output
+//     }
+//     let change_arr = denom.reduce(function(acc, curr){
+//         let value = 0
+//         while(register[curr.name] > 0 &&  change >= curr.val){
+//         change -= curr.val
+//         register[curr.name] -= curr.val
+//         value += curr.val
+//         change = Math.round(change * 100) / 100;
+//        }
+//        if(value > 0){
+//            acc.push ([curr.name, value])
+//        }
+//        return acc
+//     }, [])
+//     if(change_arr.length < 1 || change > 0){
+//         output.status = 'INSUFFICIENT_FUNDS'
+//         return output
+//     }
+//     output.status = 'OPEN'
+//     output.change = change_arr
+//     return output
+//   }
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+//   console.log(checkCashRegister(15.20, 20, [["PENNY", 1], ["NICKEL", 1], ["DIME", 1], ["QUARTER", 1], ["ONE", 10], ["FIVE", 10], ["TEN", 100], ["TWENTY", 100], ["ONE HUNDRED", 100]]));
 
 
 
